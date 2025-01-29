cfd2963230905ad3216ab5d960e5ae9b5b22f437
albinjiji989 <154455875+albinjiji989@users.noreply.github.com>
2025-01-29 21:26:20 +0530

    Update dbconnection.php

--- includes/dbconnection.php
+++ includes/dbconnection.php
@@ -1,5 +1,5 @@
 <?php
-$con=mysqli_connect("localhost", "root", "", "agmsdb");
+$con=mysqli_connect("localhost", "albin", "password", "agmsdb");
 if(mysqli_connect_errno()){
 echo "Connection Fail".mysqli_connect_error();
 }
